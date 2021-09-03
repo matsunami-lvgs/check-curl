c72d60334ebd4360a3972d08bd10510c319d6c55
matsunami-lvgs <kaito.matsunami@leverages.jp>
2021-09-03 11:45:08 +0900

    変更:HTML個別出力

--- curlALLHTML.zsh
+++ curlALLHTML.zsh
@@ -1,8 +1,8 @@
 current=$(cd $(dirname $0);pwd)
 list=(`cat ${current}/link.txt|xargs`)
-: > ${current}/result.txt
 
 for i in $list;do
 	echo $i
-	curl -LksS $i >> ${current}/result.txt
+	filename=(`echo $i|sed -E 's/[/|.|:]/_/g'`)
+	curl -LksS $i > ${current}/result/${filename}.html
 done
