d2f14c8a634b7b63ce2865e3451cd5a6c31fa7a3
matsunami-lvgs <kaito.matsunami@leverages.jp>
2023-04-17 16:56:05 +0900

    パス修正

--- curlALLHTML_sp.zsh
+++ curlALLHTML_sp.zsh
@@ -4,5 +4,5 @@ list=(`cat ${current}/link_sp.txt|xargs`)
 for i in $list;do
 	echo $i
 	filename=(`echo $i|sed -E 's/[/|.|:]/_/g'`)
-	curl -LksS  -H 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'  $i > ${current}/result/rebranding/${filename}_sp.html
+	curl -LksS  -H 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'  $i > ${current}/result/${filename}_sp.html
 done
